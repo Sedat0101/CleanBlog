baa75dd9ea4ab0581a6efd957d1efe9186461d07
sedat0101 <sfsi_0101@hotmail.com>
2024-06-21 22:19:14 +0300

    template engine

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 const express = require('express')
 const path = require('path')
+const ejs = require('ejs')
 
 const app = express()
 
+// TEMPLATE ENGINE
+app.set("view engine", "ejs")
+
+// MIDDLEWARE
 app.use(express.static('public'))
 
+
+// ROUTES
 app.get('/', (rep, res) => {
-    res.sendFile(path.resolve('temp','index.html'))
+    //res.sendFile(path.resolve('temp','index.html'))
+    res.render("index")
 })
+
+app.get('/about', (rep, res) => {
+    res.render("about")
+})
+
+app.get('/add_post', (rep, res) => {
+    res.render("add_post")
+})
+
 
 const port = 3000
 app.listen(port, () => {
