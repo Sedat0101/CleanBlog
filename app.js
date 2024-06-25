bae80ad7a29ee3a274ffc56f58ce3e42fe2c8269
sedat0101 <sfsi_0101@hotmail.com>
2024-06-25 03:57:17 +0300

    Mongo DB

--- app.js
+++ app.js
@@ -1,28 +1,42 @@
 const express = require('express')
 const path = require('path')
 const ejs = require('ejs')
+const Photo = require('./models/photo')
+const mongoose = require('mongoose')
 
 const app = express()
+
+mongoose.connect('mongodb://localhost/blog-test-db')
 
 // TEMPLATE ENGINE
 app.set("view engine", "ejs")
 
 // MIDDLEWARE
 app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
 
 
 // ROUTES
-app.get('/', (rep, res) => {
+app.get('/', async (req, res) => {
     //res.sendFile(path.resolve('temp','index.html'))
-    res.render("index")
+   const photos = await Photo.find({})
+    res.render("index", {
+        photos
+    })
 })
 
-app.get('/about', (rep, res) => {
+app.get('/about', (req, res) => {
     res.render("about")
 })
 
-app.get('/add_post', (rep, res) => {
+app.get('/add_post', (req, res) => {
     res.render("add_post")
+})
+
+app.post('/photos', async (req, res) => {
+    await Photo.create(req.body)
+    res.redirect("/")
 })
 
 
